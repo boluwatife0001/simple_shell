3b5e19a3703d0aa1f8205f7c65fe2bbe400a0e86
Kwasi12 <106727078+Kwasi12@users.noreply.github.com>
2022-11-16 10:35:37 +0000

    Update

--- builtin1.c
+++ builtin1.c
@@ -1,6 +1,6 @@
 #include "shell.h"
 
-/**
+/*
  * _myhistory - displays the history list, one command by line, preceded
  *              with line numbers, starting at 0.
  * @info: Structure containing potential arguments. Used to maintain
